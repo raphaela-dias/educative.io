e476dc986a0e90489c7a60cdba745185133d4fa7
RLMD <raphaelalmd@gmail.com>
2024-02-16 09:58:47 -0300

    Created math_quiz_app.py

---
+++ learn-python-3-from-scratch/math_quiz_app.py
@@ -0,0 +1,4 @@
+user_name = input("Please, insert your name: ")
+print("Hi", user_name, "! Welcome to the math quiz.")
+print(user_name, ", can you answer the following questions?")
+print("Congratulations", user_name, ", on scoring 5 marks!")
