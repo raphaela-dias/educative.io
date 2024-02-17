d2aeb03d93bedea7e2d33e464bd1e864fde77a0b
RLMD <raphaelalmd@gmail.com>
2024-02-17 00:49:58 -0300

    Added variable "answer" on line 6 to get the user's answer to the math question, which is predefined for testing purposes

--- learn-python-3-from-scratch/math_quiz_app.py
+++ learn-python-3-from-scratch/math_quiz_app.py
@@ -1,4 +1,8 @@
 user_name = input("Please, insert your name: ")
+
 print("Hi", user_name, "! Welcome to the math quiz.")
 print(user_name, ", can you answer the following questions?")
+
+answer = input("8 * 7 = ")
+
 print("Congratulations", user_name, ", on scoring 5 marks!")
