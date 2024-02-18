9194edb59fac0b2ac28d34061ced15c033167c64
RLMD <raphaelalmd@gmail.com>
2024-02-18 01:35:13 -0300

    Added new questions and arithmetical operations to compute the correct  answers

--- learn-python-3-from-scratch/math_quiz_app.py
+++ learn-python-3-from-scratch/math_quiz_app.py
@@ -1,8 +1,16 @@
 user_name = input("Please, insert your name: ")
 
-print("Hi", user_name, "! Welcome to the math quiz.")
-print(user_name, ", can you answer the following questions?")
+print(f"Hi {user_name}! Welcome to the math quiz.")
+print(f"{user_name}, can you answer the following questions?")
 
-answer = input("8 * 7 = ")
+answer1 = input("8 * 7 = ")
+print(f"The right answer is {8 * 7}.")
 
-print("Congratulations", user_name, ", on scoring 5 marks!")
+answer2 = input("9 + 8 = ")
+print(f"The right answer is {9 + 8}.")
+
+answer3 = input("2 - 7 = ")
+print(f"The right answer is {2 - 7}.")
+
+answer4 = input("14 / 7 = ")
+print(f"The right answer is {int(14 / 7)}.")
