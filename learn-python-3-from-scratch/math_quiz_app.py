f5d2ce3ddfeb348b4619e15603ea3cdb34a23658
RLMD <raphaelalmd@gmail.com>
2024-02-21 11:25:56 -0300

    Added greeting and farewell functions, as well as randomization of the numbers used on the questions

--- learn-python-3-from-scratch/math_quiz_app.py
+++ learn-python-3-from-scratch/math_quiz_app.py
@@ -1,3 +1,9 @@
+import random
+
+def greet_user(user_name):
+    welcome_message = (f"Hello {user_name}! Welcome to the math quiz.\nYou will be provided with 5 questions, and each carries one mark.\nGood luck!")
+    print(welcome_message)
+
 def print_question(n1, op, n2):
     question = print(f"\n{n1} {op} {n2} = ?")
     return(question)
@@ -6,30 +12,41 @@ def answer_feedback(user_answer, correct_answer):
     print(f"Your answer was {user_answer}.")
     print(f"The correct answer is {correct_answer}.")
 
-n1 = 10
-n2 = 5
+def farewell_user(user_name):
+    farewell_message = (f"\nThanks for giving the quiz {user_name}; You will get your result soon.")
+    print(farewell_message)
 
+n1 = random.randint(1, 10)
+n2 = random.randint(1, 10)
 user_name = input("Please, insert your name: ")
+greet_user(user_name)
 
-print(f"Hi {user_name}! Welcome to the math quiz.")
-print(f"{user_name}, can you answer the following questions?")
-
+n1 = random.randint(1, 10)
+n2 = random.randint(1, 10)
 print_question(n1, "+", n2)
 user_answer = input()
 correct_answer = n1 + n2
 answer_feedback(user_answer, correct_answer)
 
+n1 = random.randint(1, 10)
+n2 = random.randint(1, 10)
 print_question(n1, "-", n2)
 user_answer = input()
 correct_answer = n1 - n2
 answer_feedback(user_answer, correct_answer)
 
+n1 = random.randint(1, 10)
+n2 = random.randint(1, 10)
 print_question(n1, "*", n2)
 user_answer = input()
 correct_answer = n1 * n2
 answer_feedback(user_answer, correct_answer)
 
+n1 = random.randint(1, 10)
+n2 = random.randint(1, 10)
 print_question(n1, "/", n2)
 user_answer = input()
 correct_answer = int(n1 / n2)
 answer_feedback(user_answer, correct_answer)
+
+farewell_user(user_name)
