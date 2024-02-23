009dacb5f84ac94f76a9566b51d547d1946bf45d
RLMD <raphaelalmd@gmail.com>
2024-02-23 14:29:24 -0300

    math_quizz_app.py completed

--- learn-python-3-from-scratch/math_quiz_app.py
+++ learn-python-3-from-scratch/math_quiz_app.py
@@ -1,4 +1,10 @@
+########################
+# IMPORTED LIBRARIES
+
 import random
+
+########################
+# FUNCTION DEFINITIONS
 
 def greet_user(user_name):
     welcome_message = (f"Hello {user_name}! Welcome to the math quiz.\nYou will be provided with 5 questions, and each carries one mark.\nGood luck!")
@@ -8,45 +14,62 @@ def print_question(n1, op, n2):
     question = print(f"\n{n1} {op} {n2} = ?")
     return(question)
 
-def answer_feedback(user_answer, correct_answer):
-    print(f"Your answer was {user_answer}.")
-    print(f"The correct answer is {correct_answer}.")
+def choose_operator(op_n):
+    operator = op_n
+    if operator == 1:
+        return "-"
+    elif operator == 2:
+        return "+"
+    elif operator == 3:
+        return "*"
+    else:
+        return "/"
 
-def farewell_user(user_name):
-    farewell_message = (f"\nThanks for giving the quiz {user_name}; You will get your result soon.")
+def calculate_answer(n1, operator, n2):
+    if operator == "-":
+        return n1 - n2
+    elif operator == "+":
+        return n1 + n2
+    elif operator == "*":
+        return n1 * n2
+    else:
+        return int(n1 / n2)
+
+def answer_feedback(user_answer, correct_answer, points):
+    if user_answer == correct_answer:
+        print("Correct answer")
+        points = points + 1
+        return points
+    else:
+        print(f"Oops, that's not right.\nThe correct answer is {correct_answer}")
+        return points
+
+def farewell_user(user_name, points):
+    farewell_message = (f"\nThanks for giving the quiz {user_name}.\nYour final score is {points}.")
     print(farewell_message)
 
-n1 = random.randint(1, 10)
-n2 = random.randint(1, 10)
+########################
+# PRESET VARIABLES
+
+points = 0
+counter = 0
+
+########################
+# MAIN STRUCTURE
+
 user_name = input("Please, insert your name: ")
 greet_user(user_name)
 
-n1 = random.randint(1, 10)
-n2 = random.randint(1, 10)
-print_question(n1, "+", n2)
-user_answer = input()
-correct_answer = n1 + n2
-answer_feedback(user_answer, correct_answer)
+while counter < 4:
 
-n1 = random.randint(1, 10)
-n2 = random.randint(1, 10)
-print_question(n1, "-", n2)
-user_answer = input()
-correct_answer = n1 - n2
-answer_feedback(user_answer, correct_answer)
+    n1 = random.randint(1, 10)
+    n2 = random.randint(1, 10)
+    operator = choose_operator(random.randint(1, 4))
+    print_question(n1, operator, n2)
+    user_answer = int(input())
+    correct_answer = calculate_answer(n1, operator, n2)
+    points = answer_feedback(user_answer, correct_answer, points)
+    
+    counter += 1
 
-n1 = random.randint(1, 10)
-n2 = random.randint(1, 10)
-print_question(n1, "*", n2)
-user_answer = input()
-correct_answer = n1 * n2
-answer_feedback(user_answer, correct_answer)
-
-n1 = random.randint(1, 10)
-n2 = random.randint(1, 10)
-print_question(n1, "/", n2)
-user_answer = input()
-correct_answer = int(n1 / n2)
-answer_feedback(user_answer, correct_answer)
-
-farewell_user(user_name)
+farewell_user(user_name, points)
