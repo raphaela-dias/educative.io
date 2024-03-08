48d446460c3cea4965294d9ce41a9f59931fe74b
RLMD <raphaelalmd@gmail.com>
2024-03-08 18:53:18 -0300

    My take on the Balanced Brackets Problem It's still not working with every case. Maybe adding the if/else statements in the beginning, like I did on the first try, could solve it. I'm coming back to this later

--- python-for-programmers/005_exercise_balanced_brackets.py
+++ python-for-programmers/005_exercise_balanced_brackets.py
@@ -1,18 +1,34 @@
 def check_balance(brackets):
-    check = 0
-    for bracket in brackets:
-        if bracket == '[':
-            check += 1
 
-        elif bracket == ']':
-            check -= 1
+    half_index = int(len(brackets)/2)
 
-        if check < 0:
-            break
+    first_half_string = brackets[:half_index]
 
-    return check == 0
+    second_half_string = brackets[half_index:]
 
+    second_half_string = list(second_half_string)
 
-bracket_string = '[[]][[]]'
+    reversed_list = []
+
+    for i in range(len(second_half_string) - 1, -1, -1):
+        reversed_list.append(second_half_string[i])
+
+    second_half_string_reversed = ''.join(reversed_list)
+
+    second_half_string_reversed = second_half_string_reversed.replace('[', '_')
+
+    second_half_string_reversed = second_half_string_reversed.replace(']', '[')
+
+    second_half_string_reversed = second_half_string_reversed.replace('_', ']')
+
+    print(first_half_string)
+    print(second_half_string_reversed)
+
+    if first_half_string == second_half_string_reversed:
+        return True
+    else:
+        return False
+
+bracket_string = '[]][][[]'
 
 print(check_balance(bracket_string))
